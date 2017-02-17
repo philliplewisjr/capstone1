eec4ec1077274f23bee4d97a456e46545536cabb
Phillip Lewis <phillip.lewisjr@gmail.com>
2017-02-17 09:33:18 -0600

    login page started

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,23 @@
 
 
 app.controller('LoginCtrl', function($scope, $location) {
-  console.log('Login in controller')
+  //console.log('Login in controller')
+  $scope.bgimg = "/img/capstonebackground.jpg";
+
+  $scope.login = function () {
+      $('#modal2').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '10%', // Ending top style attribute
+        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+          //alert("Ready");
+          console.log(modal, trigger);
+        },
+        //complete: function() { alert('Closed'); } // Callback for Modal close
+      }
+    );
+  }
 })
